49763d190839c072f811a32eba71195c4c59e07d
Voloshynd <Voloshynd13@gmail.com>
2025-06-11 23:37:07 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// Write your code here
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+const numberToGuess = generateRandomNumber();
+
+function app() {
+  const userInput = generateRandomNumber();
+  const inputStr = String(userInput);
+
+  if (!checkIsValidUserInput(inputStr)) {
+    return false;
+  }
+
+  const result = getBullsAndCows(userInput, numberToGuess);
+
+  return result;
+}
+
+app();
